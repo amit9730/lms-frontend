f71026b1a50f9ce125511fd19295b93360d81779
Amit Kumar <your-github-email@example.com>
2025-10-07 15:36:11 +0530

    setup of axios instance done

--- lms-frontend/src/main.jsx
+++ lms-frontend/src/main.jsx
@@ -1,13 +1,14 @@
 // Components imports
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-router-dom';
 import App from './App.jsx'
-import './index.css'
-import {  Toaster } from 'react-hot-toast';
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <BrowserRouter>
     <App />
     <Toaster />
   </BrowserRouter>
+  </Provider>
 )
